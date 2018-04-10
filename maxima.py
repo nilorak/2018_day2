3804ec222ea3789fbe3aea749d82a946c737c89c
caro gehr <c.gehr@posteo.de>
2018-04-10 11:34:09 +0200

    Fixed maxima.py for 3.

--- maxima.py
+++ maxima.py
@@ -16,6 +16,6 @@ def find_maxima(x):
     idx = []
     for i in range(len(x)):
         # `i` is a local maximum if the signal decreases before and after it
-        if x[i-1] < x[i] and x[i+1] < x[i]:
+        if x[i-1] <= x[i] and x[i+1] <= x[i]:
             idx.append(i)
     return idx
